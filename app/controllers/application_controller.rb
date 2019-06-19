f537de255780c403d7c727a126119c86e44a26fc
James Cox <jamesncox@yahoo.com>
2019-06-19 16:52:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -11,24 +10,49 @@ class ApplicationController < Sinatra::Base
   get '/' do
     redirect to "/articles"
   end
-  
-  get '/articles' do
-    @articles = Article.all 
+
+  # index
+  get "/articles" do
+    @articles = Article.all
     erb :index
   end
-  
-  get '/articles/new' do 
-    @articles = Article.new 
+
+  # new
+  get "/articles/new" do
+    @article = Article.new
     erb :new
-  end 
-  
-  post "/articles" do
-    @article = Article.create(title: params[:title], content: params[:content])
-    redirect to "/articles/#{@article.id}"
   end
-  
-  get "articles/:id" do 
+
+  # create
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  # show
+  get "/articles/:id" do
     @article = Article.find(params[:id])
-    erb :show 
-  end 
+    erb :show
+  end
+
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
