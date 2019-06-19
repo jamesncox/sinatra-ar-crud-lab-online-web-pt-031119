43758b8ce57aeef26289bca78e791b5857d29edd
James Cox <jamesncox@yahoo.com>
2019-06-19 16:27:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,6 @@ class ApplicationController < Sinatra::Base
   
   post "/articles" do
     @article = Article.create(params)
-    redirect to "/articles/@article.id"
+    redirect to "/articles/#{@article.id}"
   end
 end
